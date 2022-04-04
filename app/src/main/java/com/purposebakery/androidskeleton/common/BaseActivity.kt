515af91f8c6e5ca17a71aad4eb080cd010bcc7cd
Oliver Metz <ometz@posteo.de>
2022-04-04 13:05:42 +1200

    Fix Compose issue in design library + BaseActivity

---
+++ app/src/main/java/com/purposebakery/androidskeleton/common/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.purposebakery.androidskeleton.common
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.purposebakery.design.theme.AndroidSkeletonTheme
+
+abstract class BaseActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidSkeletonTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Content()
+                }
+            }
+        }
+    }
+
+    @Composable
+    abstract fun Content()
+}
--- app/src/main/java/com/purposebakery/androidskeleton/features/pod/ui/PodActivity.kt
+++ app/src/main/java/com/purposebakery/androidskeleton/features/pod/ui/PodActivity.kt
@@ -1,33 +1,18 @@
 package com.purposebakery.androidskeleton.features.pod.ui
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.purposebakery.androidskeleton.common.BaseActivity
 import com.purposebakery.design.theme.AndroidSkeletonTheme
 
-class PodActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AndroidSkeletonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+class PodActivity : BaseActivity() {
+    @Composable
+    override fun Content() {
+        Greeting("Android")
     }
 }
+
 
 @Composable
 fun Greeting(name: String) {
